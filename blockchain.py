3f79ffa76bc89a98e96cda7f85f539a8677c9100
Emirhan Oğuz <160913617+emirhanoguz0@users.noreply.github.com>
2024-09-02 02:33:37 +0300

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -1,12 +1,19 @@
 import hashlib
 import json
 from time import time
+from hashlib import sha256
+from uuid import uuid4
 
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
         self.new_block(previous_hash=1, proof=100)
+
+    x = 5
+    y = 0
+    while sha256(f'{x * y}'.encode()).hexdigest()[-1] != "0":
+        y += 1
 
     def new_block(self, proof, previous_hash=None):
         # Creates a new Block and adds it to the chain
@@ -29,6 +36,21 @@ class Blockchain(object):
             'amount': amount})
 
         return self.last_block["index"] + 1
+
+    def proof_of_work(self, last_proof):
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
 
     @staticmethod
     def hash(block):
